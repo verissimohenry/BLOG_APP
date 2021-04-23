c2799c4ba6cff1d6baebd28ac2b97f87d060f8fa
karvel-code <velgithinji@gmail.com>
2021-04-23 16:47:07 +0300

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,8 @@ class CommentsController < ApplicationController
 
   private
 
-  def comment_params
-    params.require(:comment).permit(:commenter, :body)
-  end
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body, :status)
+    end
 end
