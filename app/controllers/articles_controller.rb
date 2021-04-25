180f1414d4c42a05eacb773c42b633dd9426dfaf
verissimohenry <verissimohenry04@gmail.com>
2021-04-25 20:25:43 +0100

    fix linter errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class ArticlesController < ApplicationController
   http_basic_authenticate_with name: 'dhh', password: 'secret', except: %i[index show]
 
--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module Visible
   extend ActiveSupport::Concern
 
